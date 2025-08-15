dd21493538491d5e00eb1cc903abafca95443311
DatLam <lamchandat96@gmail.com>
2025-08-15 14:37:05 +0700

    festival

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,23 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import logo from "../../assets/images/logo-festival.png";
 
 function Header() {
+  const checkboxRef = useRef(null);
+
+  const toggleMenu = () => {
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = !checkboxRef.current.checked;
+    }
+  };
+
+  const closeMenu = () => {
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = false;
+    }
+  };
+
   return (
     <header className={styles.Header}>
       <Link to="/">
@@ -13,6 +28,7 @@ function Header() {
           draggable="false"
         />
       </Link>
+
       <div className={styles.navBar}>
         <ul className={styles.nav}>
           <li>
@@ -29,6 +45,58 @@ function Header() {
           </li>
           <li>
             <Link to="/tickets">Tickets</Link>
+          </li>
+        </ul>
+      </div>
+
+      <label
+        htmlFor="nav-mobile-input"
+        className={styles.nav_bar_btn}
+        onClick={(e) => {
+          e.preventDefault();
+          toggleMenu();
+        }}
+      >
+        <i className="bx bx-menu" style={{ color: "white" }}></i>
+      </label>
+
+      <input type="checkbox" hidden id="nav-mobile-input" ref={checkboxRef} />
+
+      <label
+        htmlFor="nav-mobile-input"
+        className="nav_overlay"
+        onClick={(e) => {
+          e.preventDefault();
+          toggleMenu();
+        }}
+      ></label>
+
+      <div className="nav_mobile">
+        <ul id="nav_list">
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/place" onClick={closeMenu}>
+              Place
+            </Link>
+          </li>
+          <li>
+            <Link to="/lineup" onClick={closeMenu}>
+              Lineup
+            </Link>
+          </li>
+          <li>
+            <Link to="/gallery" onClick={closeMenu}>
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link to="/tickets" onClick={closeMenu}>
+              Tickets
+            </Link>
           </li>
         </ul>
       </div>
